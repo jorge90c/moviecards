90c24cf1edcb5a0a2c472c083e3d37a9351ba318
jorge90c <jorge_caceres@outlook.com>
2025-12-11 03:26:23 +0100

    JC:Correccion de trabajo PU

--- src/main/java/com/lauracercas/moviecards/model/Actor.java
+++ src/main/java/com/lauracercas/moviecards/model/Actor.java
@@ -45,8 +45,8 @@ public class Actor {
     }
 
     public String getName() {
-        //return name;
-        return "Hola";
+        return name;
+        //return "Hola";
     }
 
     public void setName(String name) {
